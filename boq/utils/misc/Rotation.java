f0720ecf2473e069a3a9c4ceffc0b6c4f88c2839
Bartek Bok <bartek.bok@gmail.com>
2013-06-13 22:18:20 +0200

    Additional player orientation option

--- boq/utils/misc/Rotation.java
+++ boq/utils/misc/Rotation.java
@@ -5,18 +5,33 @@ public enum Rotation {
 
     public final static Rotation[] values = values();
 
-    public static Rotation playerToSwitchOrientation(PlayerOrientation player) {
+    public static Rotation playerToSwitchOrientation(PlayerOrientation player, boolean clockwise) {
         switch (player) {
             case NORTH:
                 return R0;
             case EAST:
-                return R90;
+                return clockwise ? R90 : R270;
             case SOUTH:
                 return R180;
             case WEST:
-                return R270;
+                return clockwise ? R270 : R90;
         }
 
         return null;
+    }
+
+    public Rotation opposite() {
+        switch (this) {
+            case R0:
+                return R180;
+            case R90:
+                return R270;
+            case R180:
+                return R0;
+            case R270:
+                return R90;
+            default:
+                throw new RuntimeException("Invalid enum value");
+        }
     }
 }
