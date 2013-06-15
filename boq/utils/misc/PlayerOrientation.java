8b9925470974163e30314cf599c93a3964e3f105
Bartek Bok <bartek.bok@gmail.com>
2013-06-15 22:31:17 +0200

    Player orientation conversion

--- boq/utils/misc/PlayerOrientation.java
+++ boq/utils/misc/PlayerOrientation.java
@@ -2,6 +2,7 @@ package boq.utils.misc;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.util.MathHelper;
+import net.minecraftforge.common.ForgeDirection;
 
 public enum PlayerOrientation {
     SOUTH, WEST, NORTH, EAST;
@@ -11,5 +12,20 @@ public enum PlayerOrientation {
     public static PlayerOrientation getEntityOrientation(Entity entity) {
         int rotation = MathHelper.floor_double(entity.rotationYaw * 4.0F / 360.0F + 0.5D) & 3;
         return values[rotation];
+    }
+
+    public ForgeDirection toDirection() {
+        switch (this) {
+            case EAST:
+                return ForgeDirection.EAST;
+            case WEST:
+                return ForgeDirection.WEST;
+            case NORTH:
+                return ForgeDirection.NORTH;
+            case SOUTH:
+                return ForgeDirection.SOUTH;
+        }
+
+        throw new IllegalArgumentException("Unhandler enum value");
     }
 }
